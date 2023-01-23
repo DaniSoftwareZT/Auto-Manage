0efb3b45eac780fff66897f7e74dff563010e375
Ryan <ryan2paschen@gmail.com>
2023-01-23 14:48:21 -0500

    Service models

--- sales/api/sales_rest/models.py
+++ sales/api/sales_rest/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class SalesPerson(models.Model):
+    name = models.CharField(max_length=50)
+    employee_id = models.PositiveSmallIntegerField(unique=True)
+
+
+class PotentialCustomer(models.Model):
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    phone_number = models.CharField(max_length=11)
+
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=17, unique=True)
+
+
+class SalesRecord(models.Model):
+    automobile = models.ForeignKey(AutomobileVO, related_name="vehicle", on_delete=models.PROTECT)
+    sales_person = models.ForeignKey(SalesPerson, related_name="sales_person", on_delete=models.PROTECT)
+    customer = models.ForeignKey(PotentialCustomer, related_name="customer", on_delete=models.PROTECT)
+    price = models.PositiveSmallIntegerField()
